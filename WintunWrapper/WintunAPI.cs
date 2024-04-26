c4baa1f34997bed2267f118f5d40750d65d1b995
jkh404 <56746942+jkh404@users.noreply.github.com>
2024-04-26 13:47:52 +0800

    封装WintunAdapterWrapper
    封装WintunAdapterWrapper

--- WintunWrapper/WintunAPI.cs
+++ WintunWrapper/WintunAPI.cs
@@ -3,39 +3,32 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text;
+using Microsoft.VisualBasic;
 
-namespace  WintunWrapper
+namespace WintunWrapper
 {
-    /*
-WintunAllocateSendPacket	1	0x00006750	WintunAllocateSendPacket	
-WintunCloseAdapter	2	0x00002690	WintunCloseAdapter	
-WintunCreateAdapter	3	0x00003070	WintunCreateAdapter	
-WintunDeleteDriver	4	0x00005020	WintunDeleteDriver	
-WintunEndSession	5	0x00006550	WintunEndSession	
-WintunGetAdapterLUID	6	0x00002BF0	WintunGetAdapterLUID	
-WintunGetReadWaitEvent	7	0x000065E0	WintunGetReadWaitEvent	
-WintunGetRunningDriverVersion	8	0x00004550	WintunGetRunningDriverVersion	
-WintunOpenAdapter	9	0x00003A80	WintunOpenAdapter	
-WintunReceivePacket	10	0x000065F0	WintunReceivePacket	
-WintunReleaseReceivePacket	11	0x000066C0	WintunReleaseReceivePacket	
-WintunSendPacket	12	0x00006810	WintunSendPacket	
-WintunSetLogger	13	0x00005350	WintunSetLogger	
-WintunStartSession	14	0x000062B0	WintunStartSession	
-
-     
-     */
     public static class Const
     {
+        /// <summary>
+        /// 最大适配器名称长度
+        /// </summary>
         public const int MAX_ADAPTER_NAME = 128;
         /// <summary>
-        /// 128kiB
+        /// 环形缓冲区最小长度. 128kiB
         /// </summary>
-        public const uint WINTUN_MIN_RING_CAPACITY = 0x20000;
+        public const int WINTUN_MIN_RING_CAPACITY = 0x20000;
         /// <summary>
-        /// 64MiB
+        /// 环形缓冲区最大长度. 64MiB
         /// </summary>
-        public const uint WINTUN_MAX_RING_CAPACITY = 0x4000000;
-        
+        public const int WINTUN_MAX_RING_CAPACITY = 0x4000000;
+        /// <summary>
+        /// IP包最大长度
+        /// </summary>
+        public const int WINTUN_MAX_IP_PACKET_SIZE = 0xFFFF;
+        /// <summary>
+        /// wintundll的文件名
+        /// </summary>
+        public const string WintunDll = "wintun.dll";
     }
     /// <summary>
     /// 全局记录器回调
@@ -43,52 +36,69 @@ WintunStartSession	14	0x000062B0	WintunStartSession
     /// <param name="loggerLevel">日志等级</param>
     /// <param name="Timestamp">从1601-01-01 UTC.开始的Ticks数</param>
     /// <param name="Message">错误信息</param>
-    public unsafe delegate void WintunLoggerCallBack(WintunLoggerLevel loggerLevel,long Timestamp,char* Message);
+    public unsafe delegate void WintunLoggerCallBack(WintunLoggerLevel loggerLevel,long Timestamp,IntPtr Message);
     public unsafe static class WintunAPI
     {
-        const string WintunDll = "wintun.dll";
-
-
-        [DllImport(WintunDll, EntryPoint = "WintunAllocateSendPacket")]
-        public static extern IntPtr WintunAllocateSendPacket(TunSession session, uint PacketSize);
 
         /// <summary>
-        /// Creates a new Wintun adapter.
+        /// 创建新的 Wintun 适配器。
         /// </summary>
-        /// <param name="Name">The requested name of the adapter. Zero-terminated string of up to MAX_ADAPTER_NAME-1 characters.</param>
-        /// <param name="TunnelType"> Name of the adapter tunnel type. Zero-terminated string of up to MAX_ADAPTER_NAME-1 characters.</param>
-        /// <param name="RequestedGUID">The GUID of the created network adapter, which then influences NLA generation deterministically. If it is set to NULL, the GUID is chosen by the system at random, and hence a new NLA entry is created for each new adapter. It is called "requested" GUID because the API it uses is completely undocumented, and so there could be minor interesting complications with its usage.</param>
+        /// <param name="Name">适配器的请求名称。最多 MAX_ADAPTER_NAME-1 个字符的零终止字符串。</param>
+        /// <param name="TunnelType">适配器隧道类型的名称。最多 MAX_ADAPTER_NAME-1 个字符的零终止字符串。</param>
+        /// <param name="RequestedGUID">
+        /// 创建的网络适配器的 GUID，然后确定性地影响 NLA 生成。
+        /// 如果设置为 NULL，则系统会随机选择 GUID，从而为每个新适配器创建一个新的 NLA 条目。
+        /// 它之所以称为“请求的”GUID，是因为它使用的 API 是完全未记录的，因此其使用可能会有一些有趣的复杂情况。</param>
         /// <returns></returns>
-        [DllImport(WintunDll, EntryPoint = "WintunCreateAdapter",CharSet = CharSet.Unicode)]
-        public static extern  WintunAdapter* WintunCreateAdapter(string Name, string TunnelType, IntPtr RequestedGUID);
+        [DllImport(Const.WintunDll, EntryPoint = "WintunCreateAdapter",CharSet = CharSet.Unicode)]
+        public static extern  IntPtr WintunCreateAdapter(string Name, string TunnelType, IntPtr RequestedGUID);
 
         /// <summary>
-        /// Opens an existing Wintun adapter.
+        /// 打开现有的 Wintun 适配器。
         /// </summary>
-        /// <param name="Name">The requested name of the adapter. Zero-terminated string of up to MAX_ADAPTER_NAME-1 characters.</param>
+        /// <param name="Name">适配器的请求名称。最多 MAX_ADAPTER_NAME-1 个字符的零终止字符串。</param>
         /// <returns></returns>
-        [DllImport(WintunDll, EntryPoint = "WintunOpenAdapter", CharSet = CharSet.Unicode)]
-        public static extern WintunAdapter* WintunOpenAdapter(string Name);
+        [DllImport(Const.WintunDll, EntryPoint = "WintunOpenAdapter", CharSet = CharSet.Unicode)]
+        public static extern IntPtr WintunOpenAdapter(string Name);
 
-        //void WintunCloseAdapter (WINTUN_ADAPTER_HANDLE Adapter)
-        [DllImport(WintunDll, EntryPoint = "WintunCloseAdapter")]
-        public static extern void WintunCloseAdapter(WintunAdapter* Adapter);
+        /// <summary>
+        /// 释放 Wintun 适配器资源，如果适配器是使用 WintunCreateAdapter 创建的，则删除适配器。
+        /// </summary>
+        /// <param name="Adapter">使用 WintunCreateAdapter 或 WintunOpenAdapter 获取的适配器句柄。</param>
+        [DllImport(Const.WintunDll, EntryPoint = "WintunCloseAdapter")]
+        public static extern void WintunCloseAdapter(IntPtr Adapter);
+        /// <summary>
+        /// 如果不再使用适配器，则删除 Wintun 驱动程序。
+        /// </summary>
+        /// <returns>如果函数成功，则返回值为非零。如果函数失败，则返回值为零。  </returns>
 
-        [DllImport(WintunDll, EntryPoint = "WintunDeleteDriver")]
+        [DllImport(Const.WintunDll, EntryPoint = "WintunDeleteDriver")]
         public static extern int WintunDeleteDriver();
+        /// <summary>
+        /// 返回适配器的 LUID。
+        /// </summary>
+        /// <param name="Adapter">使用 WintunOpenAdapter 或 WintunCreateAdapter 获取的适配器句柄</param>
+        /// <param name="Luid">指向 LUID 的指针以接收适配器 LUID。</param>
 
-        [DllImport(WintunDll, EntryPoint = "WintunGetAdapterLUID")]
-        public static extern void WintunGetAdapterLUID(WintunAdapter* Adapter, NetLUIDLH* Luid);
+        [DllImport(Const.WintunDll, EntryPoint = "WintunGetAdapterLUID")]
+        public static extern void WintunGetAdapterLUID(IntPtr Adapter, IntPtr Luid);
 
-
-        [DllImport(WintunDll, EntryPoint = "WintunGetRunningDriverVersion")]
+        /// <summary>
+        /// 确定当前加载的 Wintun 驱动程序的版本。
+        /// 若要获取扩展的错误信息，请调用 GetLastError。可能的错误包括： ERROR_FILE_NOT_FOUND Wintun 未加载
+        /// </summary>
+        /// <returns>如果函数成功，则返回值为版本号。如果函数失败，则返回值为零。</returns>
+        [DllImport(Const.WintunDll, EntryPoint = "WintunGetRunningDriverVersion")]
         public static extern int WintunGetRunningDriverVersion();
         /// <summary>
         /// 设置记录器回调函数。
         /// </summary>
-        /// <param name="wintunLoggerCallBack">指向回调函数的指针，用作新的全局记录器。NewLogger 可以同时从各种线程调用。如果日志记录需要序列化，则必须在 NewLogger 中处理序列化。设置为 NULL 以禁用。</param>
+        /// <param name="wintunLoggerCallBack">
+        /// 指向回调函数的指针，用作新的全局记录器。NewLogger 可以同时从各种线程调用。
+        /// 如果日志记录需要序列化，则必须在 NewLogger 中处理序列化。设置为 NULL 以禁用。
+        /// </param>
         /// <returns></returns>
-        [DllImport(WintunDll, EntryPoint = "WintunSetLogger", CharSet = CharSet.Unicode)]
+        [DllImport(Const.WintunDll, EntryPoint = "WintunSetLogger", CharSet = CharSet.Unicode)]
         public static extern int WintunSetLogger(WintunLoggerCallBack wintunLoggerCallBack);
 
         /// <summary>
@@ -98,14 +108,76 @@ WintunStartSession	14	0x000062B0	WintunStartSession
         /// <param name="Capacity">容量。必须介于 WINTUN_MIN_RING_CAPACITY 和 WINTUN_MAX_RING_CAPACITY 之间（包括）并且必须是 2 的幂。</param>
         /// <returns>Wintun 会话句柄。必须与 WintunEndSession 一起使用。如果函数失败，则返回值为 NULL。</returns>
 
-        [DllImport(WintunDll, EntryPoint = "WintunStartSession")]
+        [DllImport(Const.WintunDll, EntryPoint = "WintunStartSession")]
         public static extern TunSession* WintunStartSession(WintunAdapter* Adapter, uint Capacity);
+        /// <summary>
+        /// 结束 Wintun 会话。
+        /// </summary>
+        /// <param name="Session">使用 WintunStartSession 获取的 Wintun 会话句柄</param>
+        /// <returns></returns>
+        [DllImport(Const.WintunDll, EntryPoint = "WintunEndSession")]
+        public static extern TunSession* WintunEndSession(TunSession* Session);
+        //HANDLE WintunGetReadWaitEvent (WINTUN_SESSION_HANDLE Session)
+        /// <summary>
+        /// 获取 Wintun 会话的读取-等待事件句柄。
+        /// </summary>
+        /// <param name="Session">使用 WintunStartSession 获取的 Wintun 会话句柄</param>
+        /// <returns>
+        /// 用于接收事件句柄的指针，用于在读取时等待可用数据。
+        /// 如果 WintunReceivePackets 返回ERROR_NO_MORE_ITEMS（在重负载下旋转一段时间后），请等待此事件发出信号，然后再重试 WintunReceivePackets。
+        /// 不要在此事件上调用 CloseHandle - 它由会话管理。</returns>
+        [DllImport(Const.WintunDll, EntryPoint = "WintunGetReadWaitEvent")]
+        public static extern IntPtr WintunGetReadWaitEvent(TunSession* Session);
 
+        /// <summary>
+        /// 检索一个或数据包。使用数据包内容后，使用从此函数返回的数据包调用 WintunReleaseReceivePacket 以释放内部缓冲区。此函数是线程安全的。
+        /// </summary>
+        /// <param name="Session">使用 WintunStartSession 获取的 Wintun 会话句柄</param>
+        /// <param name="PacketSize">用于接收数据包大小的指针。</param>
+        /// <returns>指向第 3 层 IPv4 或 IPv6 数据包的指针。客户可以随意修改其内容。如果函数失败，则返回值为 NULL。</returns>
+        [DllImport(Const.WintunDll, EntryPoint = "WintunReceivePacket")]
+        public static extern byte* WintunReceivePacket(TunSession* Session,ref uint PacketSize);
+        /// <summary>
+        /// 在客户端处理收到的数据包后释放内部缓冲区。此函数是线程安全的。
+        /// </summary>
+        /// <param name="Session">使用 WintunStartSession 获取的 Wintun 会话句柄</param>
+        /// <param name="Packet">使用 WintunReceivePacket 获取的数据包</param>
+        [DllImport(Const.WintunDll, EntryPoint = "WintunReleaseReceivePacket")]
+        public static extern void WintunReleaseReceivePacket(TunSession* Session, byte* Packet);
+        //BYTE* WintunAllocateSendPacket (WINTUN_SESSION_HANDLE Session, DWORD PacketSize)
+        /// <summary>
+        /// 为要发送的数据包分配内存。内存中填满数据包数据后，调用 WintunSendPacket 发送并释放内部缓冲区。
+        /// WintunAllocateSendPacket 是线程安全的，调用的 WintunAllocateSendPacket 顺序定义数据包发送顺序。
+        /// </summary>
+        /// <param name="Session">使用 WintunStartSession 获取的 Wintun 会话句柄</param>
+        /// <param name="PacketSize">确切的数据包大小。必须小于或等于 WINTUN_MAX_IP_PACKET_SIZE。</param>
+        /// <returns>
+        /// 返回指向内存的指针，用于准备第 3 层 IPv4 或 IPv6 数据包以进行发送。
+        /// 如果函数失败，则返回值为 NULL。
+        /// 若要获取扩展的错误信息，请调用 GetLastError。可能的错误包括：ERROR_HANDLE_EOF Wintun 适配器正在终止;ERROR_BUFFER_OVERFLOW Wintun 缓冲区已满;
+        /// </returns>
+        [DllImport(Const.WintunDll, EntryPoint = "WintunReleaseReceivePacket")]
+        public static extern byte* WintunAllocateSendPacket(TunSession* Session, uint PacketSize);
 
-        public static IntPtr GetPtr<T>(this T valueObj)where T:struct
+        /// <summary>
+        /// 发送数据包并释放内部缓冲区。WintunSendPacket 是线程安全的，但调用的 WintunAllocateSendPacket 顺序定义数据包发送顺序。
+        /// 这意味着尚不能保证在 WintunSendPacket 中发送数据包。
+        /// </summary>
+        /// <param name="Session">使用 WintunStartSession 获取的 Wintun 会话句柄</param>
+        /// <param name="Packet">使用 WintunAllocateSendPacket 获取的数据包</param>
+        [DllImport(Const.WintunDll, EntryPoint = "WintunSendPacket")]
+        public static extern void WintunSendPacket(TunSession* Session, byte* Packet);
+
+        public static IntPtr GetPtr<T>(this T? valueObj)where T:struct
         {
+            if (valueObj==null) throw new ArgumentNullException(nameof(valueObj));
             IntPtr intPtr = Marshal.AllocHGlobal(Marshal.SizeOf(valueObj));
             Marshal.StructureToPtr(valueObj, intPtr,false);
+            return intPtr;
+        }
+        public static IntPtr GetPtr<T>() where T : struct
+        {
+            IntPtr intPtr = Marshal.AllocHGlobal(Marshal.SizeOf<T>());
             return intPtr;
         }
         public static T? ToStruct<T>(this IntPtr ptr) where T : struct
@@ -125,17 +197,8 @@ WintunStartSession	14	0x000062B0	WintunStartSession
 
     public enum WintunLoggerLevel:int
     {
-        /// <summary>
-        /// Informational
-        /// </summary>
         INFO,
-        /// <summary>
-        /// Warning
-        /// </summary>
         WARN,
-        /// <summary>
-        /// Error
-        /// </summary>
         ERR,        
     }
     [StructLayout(LayoutKind.Explicit, Size = 8)]
---
+++ WintunWrapper/WintunAdapterWrapper.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace WintunWrapper
+{
+    public class WintunAdapterWrapper:IDisposable
+    {
+        public delegate void WintunLoggerCallBack(WintunLoggerLevel loggerLevel, DateTime dateTime, string? Message);
+        public event WintunLoggerCallBack? OnLog;
+        private IntPtr _AdapterPtr=IntPtr.Zero;
+        public readonly string Name;
+        public readonly string TunnelType;
+
+        public bool IsOpen { get; private set; }
+        private bool disposedValue;
+
+        private WintunAdapterWrapper(IntPtr adapterPtr, string name,string tunnelType, bool isOpen)
+        {
+            if (adapterPtr==IntPtr.Zero) throw new ArgumentNullException(nameof(adapterPtr));
+            _AdapterPtr=adapterPtr;
+            Name=name;
+            TunnelType=tunnelType;
+            IsOpen=isOpen;
+            if(IsOpen) WintunAPI.WintunSetLogger(DefaultWintunLoggerCallBack);
+        }
+        public void Open()
+        {
+            _AdapterPtr=WintunAPI.WintunOpenAdapter(Name);
+            if (_AdapterPtr!=IntPtr.Zero) IsOpen=true;
+            if (IsOpen) WintunAPI.WintunSetLogger(DefaultWintunLoggerCallBack);
+        }
+        public void Close()
+        {
+            if (_AdapterPtr==IntPtr.Zero) throw new InvalidOperationException();
+            WintunAPI.WintunCloseAdapter(_AdapterPtr);
+            IsOpen=false;
+        }
+        public NetLUIDLH? GetLUID()
+        {
+            if (_AdapterPtr==IntPtr.Zero) throw new InvalidOperationException();
+            var Intptr=WintunAPI.GetPtr<NetLUIDLH>();
+            WintunAPI.WintunGetAdapterLUID(_AdapterPtr, Intptr);
+            if (Intptr!=IntPtr.Zero)
+            {
+                return Intptr.ToStruct<NetLUIDLH>();
+            }
+            else
+            {
+                //// 假设你的 IntPtr 对象叫做 quitEventPtr
+                //IntPtr quitEventPtr = IntPtr.Zero; // 你的 IntPtr 对象
+
+                //// 创建一个 AutoResetEvent 对象，并将其 SafeWaitHandle 属性设置为你的 IntPtr 对象
+                //AutoResetEvent quitEvent = new AutoResetEvent(false);
+                //quitEvent.SafeWaitHandle = new Microsoft.Win32.SafeHandles.SafeWaitHandle(quitEventPtr, false);
+                //// 等待 quitEvent
+                //bool signaled = quitEvent.WaitOne(1000); // 等待 1 秒
+                return null;
+            }
+        }
+        public int GetRunningDriverVersion()
+        {
+            if (_AdapterPtr==IntPtr.Zero) throw new InvalidOperationException();
+            return WintunAPI.WintunGetRunningDriverVersion();
+        }
+        
+        private void DefaultWintunLoggerCallBack(WintunLoggerLevel loggerLevel, long timestamp, IntPtr Message)
+        {
+
+            var now = new DateTime(1601, 1, 1, 0, 0, 0, DateTimeKind.Local).AddTicks(timestamp);
+            string? msg=null;
+            if (Message!=IntPtr.Zero)
+            {
+                msg=Marshal.PtrToStringUni(Message);
+            }
+            OnLog?.Invoke(loggerLevel, now, msg);
+        }
+
+        public static WintunAdapterWrapper Create(string Name="Default", string TunnelType= "Default", Guid? RequestedGUID=null)
+        {
+            if (RequestedGUID==null) RequestedGUID=Guid.NewGuid();
+            IntPtr GUIDPtr = RequestedGUID.GetPtr();
+            var adapterPtr = WintunAPI.WintunCreateAdapter(Name, TunnelType, GUIDPtr);
+            return new WintunAdapterWrapper(adapterPtr, Name, TunnelType, false);
+        }
+        /// <summary>
+        /// 如果不再使用，调用该方法删除驱动
+        /// </summary>
+        /// <returns>执行结果</returns>
+        public static bool DeleteDriver()
+        {
+            return WintunAPI.WintunDeleteDriver()!=0;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: 释放托管状态(托管对象)
+                }
+                
+                // TODO: 释放未托管的资源(未托管的对象)并重写终结器
+                // TODO: 将大型字段设置为 null
+                disposedValue=true;
+                if (_AdapterPtr!=IntPtr.Zero)
+                {
+                    WintunAPI.WintunCloseAdapter(_AdapterPtr);
+                    _AdapterPtr=IntPtr.Zero;
+                }
+            }
+        }
+
+        // TODO: 仅当“Dispose(bool disposing)”拥有用于释放未托管资源的代码时才替代终结器
+        ~WintunAdapterWrapper()
+        {
+            // 不要更改此代码。请将清理代码放入“Dispose(bool disposing)”方法中
+            Dispose(disposing: false);
+        }
+        public void Dispose()
+        {
+            // 不要更改此代码。请将清理代码放入“Dispose(bool disposing)”方法中
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
